4c0de2a9d948eeb0ddee1c7efc601d54ccf2f4dd
7-Zark-7 <1085655+zx8086@users.noreply.github.com>
2024-05-16 21:55:04 +0200

    Added third draft of DROP INDEX Added Counters for successful / failed

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import { connectToCouchbase } from './src/lib/couchbaseConnector.ts';
 import type {Cluster, PingResult, QueryResult} from 'couchbase';
 
 let cluster: Cluster;
+let successfulDrops = 0;
+let failedDrops = 0;
 
 type DropIndexConfig = {
     bucketName: string;
@@ -11,23 +13,28 @@ type DropIndexConfig = {
 async function getIndexesToDrop(): Promise<DropIndexConfig[]> {
     const query: string = `
         SELECT 
-          (SELECT i_inner.name, i_inner.keyspace_id
-          FROM system:indexes AS i_inner
-          WHERE i_inner.metadata.last_scan_time IS NULL AND ANY v IN ["orders"] SATISFIES i_inner.keyspace_id LIKE "%" || v || "%" END) as indexes
-        FROM system:indexes AS i
-        WHERE i.metadata.last_scan_time IS NULL AND ANY v IN ["orders"] SATISFIES i.keyspace_id LIKE "%" || v || "%" END;
+  (SELECT i_inner.name, i_inner.keyspace_id, i_inner.\`namespace\`, i_inner.namespace_id, i_inner.state
+  FROM system:indexes AS i_inner
+  WHERE i_inner.metadata.last_scan_time IS NULL AND ANY v IN ["travel-sample"] SATISFIES i_inner.keyspace_id LIKE v || "%" END) as last_scan_null,
+  COUNT(*) AS total
+FROM system:indexes AS i
+WHERE i.metadata.last_scan_time IS NULL AND ANY v IN ["travel-sample"] SATISFIES i.keyspace_id LIKE v || "%" END;
     `;
     let result: QueryResult = await cluster.query(query);
-    console.log("Indexes to drop...",result);
+    console.log(JSON.stringify(result, null, 2));
 
     const dropIndexConfigs: DropIndexConfig[] = [];
 
     for(let row of result.rows){
-        for(let indexInfo of row.indexes){
-            dropIndexConfigs.push({
-                bucketName: indexInfo.keyspace_id,
-                indexName: indexInfo.name
-            });
+        if(row.last_scan_null){
+            for(let indexInfo of row.last_scan_null){
+                dropIndexConfigs.push({
+                    bucketName: indexInfo.keyspace_id,
+                    indexName: indexInfo.name
+                });
+            }
+        } else {
+            console.log('No indexes to drop for this row');
         }
     }
 
@@ -36,6 +43,7 @@ async function getIndexesToDrop(): Promise<DropIndexConfig[]> {
 
 async function dropIndices(dropIndexConfigs: DropIndexConfig[]): Promise<void> {
     for (const config of dropIndexConfigs) {
+        console.log(`Dropping index: ${config.indexName} of bucket: ${config.bucketName}`);
         await dropIndex(config.bucketName, config.indexName);
     }
 }
@@ -52,17 +60,21 @@ async function pingCluster(): Promise<void> {
 async function dropIndex(bucketName: string, indexName: string): Promise<void> {
     try {
         let queryIndex : string = `DROP INDEX \`${bucketName}\`.\`${indexName}\``;
+        console.log(queryIndex)
         let result: QueryResult = await cluster.query(queryIndex);
         console.log(JSON.stringify(result, null, 2));
         console.log(`Index ${indexName} dropped successfully`);
+        successfulDrops++; // increment successful drops by 1
     } catch (error: any) {
         if (error.name === 'IndexNotFoundError') {
             console.log(`Index ${indexName} does not exist, hence cannot be dropped.`);
         } else {
             console.error(`Error dropping index ${indexName}:`, error);
         }
+        failedDrops++; // increment failed drops by 1
     }
 }
+
 
 async function queryCapella(query: string): Promise<void> {
     try {
@@ -81,10 +93,10 @@ async function main() :Promise<void> {
         cluster = result.cluster;
         const dropIndicesConfig: DropIndexConfig[] = await getIndexesToDrop();
         await dropIndices(dropIndicesConfig);
-    } catch (error: any) {
-        console.log(error);
-        throw error;
     } finally {
+        console.log(`Successful Index Deletions: ${successfulDrops}`);
+        console.log(`Failed Index Deletions: ${failedDrops}`);
+        console.log(`Total Attempted Deletions: ${successfulDrops + failedDrops}`);
         console.log("Exiting...");
         process.exit(0);
     }
